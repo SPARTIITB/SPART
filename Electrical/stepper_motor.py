08279b49e6e75fe5c35df0c53b6aed0b4e922aa3
SohamCP <sohamchetanpatil1@gmail.com>
2024-12-20 17:53:35 +0530

    FIRST COMMIT

---
+++ Electrical/stepper_motor.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class StepperMotor:
+    pass
+
+
+def main():
+    print("Hello World")
+    GPIO.setmode(GPIO.BOARD)
+
+
+if __name__ == "__main__":
+    main()
